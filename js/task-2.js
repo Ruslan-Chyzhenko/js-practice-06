646b6785df10acdcab202a7267c7e15e40011e87
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-06-20 19:50:30 +0200

    002

--- js/task-2.js
+++ js/task-2.js
@@ -1,19 +1,5 @@
 // Main function
-class Storage {
-  #items;
-  constructor( items ) {
-    this.#items = items;
-    }
-  getItems() {
-    return this.#items;
-  }
-  addItem(newItem) {
-    this.#items.push(newItem);
-  }
-  removeItem(itemToRemove) {
-    this.#items = this.#items.filter(item => item !== itemToRemove);
-  }
-  }
+
   
 // Text calls
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
---
+++ js/task-3+.js
@@ -0,0 +1,20 @@
+// const user = {
+//   username: "Victor",
+//   showName() {
+// 		// ❌ Віктор біжить швидко, тому що Віктор намагається зловити поїзд
+//     console.log(user.username);
+//   },
+// };
+
+// user.showName();
+
+// this
+const user = {
+  username: "Victor",
+  showName() {
+		// ✅ Віктор біжить швидко, тому що він (this) намагається зловити поїзд.
+    console.log(this.username);
+  },
+};
+
+user.showName();
